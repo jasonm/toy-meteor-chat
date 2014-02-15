0a1fbc62c991fddaf38fe5f1d6ef9c146ea53b0f
Jason Morrison <jason.p.morrison@gmail.com>
2014-02-14 23:45:41 -0800

    chat

--- mchat.js
+++ mchat.js
@@ -1,19 +1,44 @@
+Messages = new Meteor.Collection('messages');
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to mchat.";
+  var room = 'lobby';
+
+  Template.chat.helpers({
+    room: room,
+    messages: function() {
+      return Messages.find({ room: room });
+    }
+  });
+
+  var currentUserName = 'Jason';
+
+  var sendChat = function(room, message) {
+    Messages.insert({ room: room, author: currentUserName, body: message, date: new Date() });
   };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.chat.events({
+    'keyup input[name=new-message]': function(e) {
+      if (e.keyCode === 13) {
+        sendChat(room, e.target.value);
+        e.target.value = '';
+      }
     }
   });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
+    if (Messages.find().count() === 0) {
+      Messages.insert({ date: new Date(), room: 'lobby', author: 'Jason', body: 'Hello' });
+      Messages.insert({ date: new Date(), room: 'lobby', author: 'Jason', body: 'welcome to the chat' });
+      Messages.insert({ date: new Date(), room: 'lobby', author: 'Jason', body: 'come in!' });
+      Messages.insert({ date: new Date(), room: 'otherchat', author: 'Jason', body: 'OTHER CHAT Hello' });
+      Messages.insert({ date: new Date(), room: 'otherchat', author: 'Jason', body: 'OTHER CHAT welcome to the chat' });
+      Messages.insert({ date: new Date(), room: 'otherchat', author: 'Jason', body: 'OTHER CHAT come in!' });
+    }
     // code to run on server at startup
   });
 }
+
+
+
